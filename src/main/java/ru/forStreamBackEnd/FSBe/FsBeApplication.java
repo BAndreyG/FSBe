307216332b5b83637db7d19f55a21e93d0f9bd98
BAndreyG <wolf-irbis@yandex.ru>
2020-03-19 18:05:54 +1000

    first

---
+++ src/main/java/ru/forStreamBackEnd/FSBe/FsBeApplication.java
@@ -0,0 +1,13 @@
+package ru.forStreamBackEnd.FSBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/forStreamBackEnd/FSBe/FsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.forStreamBackEnd.FSBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
